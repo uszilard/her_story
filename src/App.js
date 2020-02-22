fbf3eaea179029e2f994fee5033fd3a4ce5443cd
Szilard Udroiu <uszilard@gmail.com>
2020-02-22 13:05:45 +0100

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import BooksList from "./pages/BooksList";
-import NewsLetter from "./pages/NewsLetter";
-import NoMatch from "./pages/NoMatch";
+//import BooksList from "./pages/BooksList";
+//import NewsLetter from "./pages/NewsLetter";
+//import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import EventsList from "./pages/EventsList";
-import { StateProvider } from './utils/GlobalState';
+//import EventsList from "./pages/EventsList";
+
 
 function App() {
   return (
     <Router>
 
-      <StateProvider>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/events" component={EventsList} />
-          <Route exact path="/books" component={BooksList} />
-          <Route exact path="/newsletter" component={NewsLetter} />
-          <Route component={NoMatch} />
-        </Switch>
-      </StateProvider>
+
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        {/*<Route exact path="/events" component={EventsList} />
+        <Route exact path="/books" component={BooksList} />
+        <Route exact path="/newsletter" component={NewsLetter} />
+        <Route component={NoMatch} />*/}
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+          </head>
+          <body>
+
+          </body>
+        </html>-->
+      </Switch>
+
 
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,9 @@
-import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import CreatePostForm from "../components/CreatePostForm";
-import PostsList from "../components/PostsList";
+//import React from "react";
+
+
 
 function Home() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-6">
-          <CreatePostForm />
-        </Col>
-        <Col size="md-6 sm-12">
-          <PostsList />
-        </Col>
-      </Row>
-    </Container>
+  return ('hello world'
   );
 }
 
