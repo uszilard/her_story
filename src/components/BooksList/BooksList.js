7432755c7ebc29d9a7d0894c9300889de3edc0cc
snufka <52801250+snufka@users.noreply.github.com>
2020-02-25 19:51:59 +0100

    api work

---
+++ src/components/BooksList/BooksList.js
@@ -0,0 +1,24 @@
+import React from "react";
+/* title: "", //volumeInfo.title
+    auther: "", ///volumeInfo.auther
+    description: "", //volumeInfo.description
+    img: "", //imageLinks.smallThumbnail
+    link: "", //infoLink*/
+
+function ResultBookList(props) {
+    return (
+        <ul className="list-group">
+            {props.results.map(result => (
+                <li className="list-group-item" key={result.volumeInfo.title}>
+                    <h4>{result.volumeInfo.title}</h4>
+                    <p>By:{result.volumeInfo.auther}</p>
+                    <img alt={result.volumeInfo.title} className="img-fluid" src={result.imageLinks.smallThumbnail} />
+                    <p>{result.volumeInfo.description}</p>
+                    <a href={result.infoLink}>more info</a>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default ResultBookList;
