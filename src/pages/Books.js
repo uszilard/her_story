8ded96cda50e9d42ca3b4f4bf9ce38dc3b1199ca
snufka <52801250+snufka@users.noreply.github.com>
2020-02-25 21:12:28 +0100

    api workin

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import { makeStyles } from '@material-ui/core/styles';
-import APIBooks from "../utils/APIBooks"
 
+import axios from 'axios'
+
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q="
 
 
 const bookStyles = makeStyles({
@@ -17,26 +19,38 @@ const bookStyles = makeStyles({
 
 function Books() {
 
-    const [data, setData] = useState({ hits: [] });
+    const [data, setData] = useState([]);
 
 
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await APIBooks.search()
-            setData(result.data);
+            const result = await axios.get(BASEURL + "women" + "?maxResults=10?printType=book");
+            console.log(result)
+            setData(result.data.items);
         };
         fetchData();
     }, []);
 
     const classes = bookStyles();
-
-    console.log(data)
+    console.log("dsafdshj", data);
     return (
         <Container fluid>
             <nav>
                 <h1 className={classes.logo} id='homePageLogo'>Books</h1>
             </nav>
+
+            <ul className="list-group">
+                {data.length && data.map(result => (
+                    <li className="list-group-item" key={result.volumeInfo.title}>
+                        <h4>{result.volumeInfo.title}</h4>
+                        <p>By:{result.volumeInfo.auther}</p>
+                        <img alt={result.volumeInfo.title} className="img-fluid" src={result.volumeInfo.imageLinks.smallThumbnail} />
+                        <p>{result.volumeInfo.description}</p>
+                        <a href={result.infoLink}>more info</a>
+                    </li>
+                ))}
+            </ul>
 
         </Container>
     );
