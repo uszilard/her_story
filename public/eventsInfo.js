0c93901e7b50e7b79f3516dc0e3a62c12c132179
snufka <52801250+snufka@users.noreply.github.com>
2020-02-27 20:32:34 +0100

    event info object

---
+++ public/eventsInfo.js
@@ -0,0 +1,61 @@
+const eventsInfo = [
+    {
+        eventTitle: "Women's Empowerment Brunch - Berlin: Self-Worth Edition",
+        host: "Female Leaders Berlin",
+        Date: "Mon, March 2",
+        startHour: "10:00 AM ",
+        endHour: "11:30 AM",
+        eventDiscription: "How to start a business in Berlin? What is Finanzamt and what should I know before I go there? What to know before I start? Where to find funding? Is my business plan doable?",
+        img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F94232215%2F138246703928%2F1%2Foriginal.20200226-134618?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C68%2C1920%2C960&s=cb00eaea66d64903da5bac9b7c1f31f3",
+
+        eventLink: "https://www.eventbrite.com/e/female-leaders-berlin-tickets-92711517645?aff=ebdssbdestsearch"
+
+    },
+    {
+        eventTitle: "Women's Empowerment Brunch - Berlin: Self-Worth Edition",
+        host: "Female Leaders Berlin",
+        Date: "Mon, March 2",
+        startHour: "10:00 AM ",
+        endHour: "11:30 AM",
+        eventDiscription: "How to start a business in Berlin? What is Finanzamt and what should I know before I go there? What to know before I start? Where to find funding? Is my business plan doable?",
+        img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F94232215%2F138246703928%2F1%2Foriginal.20200226-134618?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C68%2C1920%2C960&s=cb00eaea66d64903da5bac9b7c1f31f3",
+
+        eventLink: "https://www.eventbrite.com/e/female-leaders-berlin-tickets-92711517645?aff=ebdssbdestsearch"
+
+    }, {
+        eventTitle: "Women's Empowerment Brunch - Berlin: Self-Worth Edition",
+        host: "Female Leaders Berlin",
+        Date: "Mon, March 2",
+        startHour: "10:00 AM ",
+        endHour: "11:30 AM",
+        eventDiscription: "How to start a business in Berlin? What is Finanzamt and what should I know before I go there? What to know before I start? Where to find funding? Is my business plan doable?",
+        img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F94232215%2F138246703928%2F1%2Foriginal.20200226-134618?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C68%2C1920%2C960&s=cb00eaea66d64903da5bac9b7c1f31f3",
+
+        eventLink: "https://www.eventbrite.com/e/female-leaders-berlin-tickets-92711517645?aff=ebdssbdestsearch"
+
+    }, {
+        eventTitle: "Women's Empowerment Brunch - Berlin: Self-Worth Edition",
+        host: "Female Leaders Berlin",
+        Date: "Mon, March 2",
+        startHour: "10:00 AM ",
+        endHour: "11:30 AM",
+        eventDiscription: "How to start a business in Berlin? What is Finanzamt and what should I know before I go there? What to know before I start? Where to find funding? Is my business plan doable?",
+        img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F94232215%2F138246703928%2F1%2Foriginal.20200226-134618?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C68%2C1920%2C960&s=cb00eaea66d64903da5bac9b7c1f31f3",
+
+        eventLink: "https://www.eventbrite.com/e/female-leaders-berlin-tickets-92711517645?aff=ebdssbdestsearch"
+
+    },
+    {
+        eventTitle: "Women's Empowerment Brunch - Berlin: Self-Worth Edition",
+        host: "Female Leaders Berlin",
+        Date: "Mon, March 2",
+        startHour: "10:00 AM ",
+        endHour: "11:30 AM",
+        eventDiscription: "How to start a business in Berlin? What is Finanzamt and what should I know before I go there? What to know before I start? Where to find funding? Is my business plan doable?",
+        img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F94232215%2F138246703928%2F1%2Foriginal.20200226-134618?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C68%2C1920%2C960&s=cb00eaea66d64903da5bac9b7c1f31f3",
+
+        eventLink: "https://www.eventbrite.com/e/female-leaders-berlin-tickets-92711517645?aff=ebdssbdestsearch"
+
+    }];
+
+export default eventsInfo;
