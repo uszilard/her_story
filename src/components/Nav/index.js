b403c8383a15600a3b9a1b24916669ce4592c889
snufka <52801250+snufka@users.noreply.github.com>
2020-03-03 21:20:33 +0100

    navbar work

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import HomeIcon from "@material-ui/icons/Home";
-import PeopleIcon from "@material-ui/icons/People";
+//import HomeIcon from "@material-ui/icons/Home";
+//import PeopleIcon from "@material-ui/icons/People";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
 import EmailIcon from "@material-ui/icons/Folder";
 
@@ -12,8 +12,13 @@ import styled from "styled-components"
 import { Link } from "react-router-dom";
 
 import { FaFemale } from 'react-icons/fa';
+import { MdPeople } from 'react-icons/md';
+import { GiWhiteBook } from 'react-icons/gi';
+import { MdEmail } from 'react-icons/md';
+
 
 const pink = "#CC0E70"
+const navFontSize = "20px"
 
 const useStyles = makeStyles({
   root: {
@@ -43,16 +48,16 @@ export default function SimpleBottomNavigation() {
       className={classes.root}
     >
       <StyledLink to='/'>
-        <BottomNavigationAction showLabel='true' label="Home" icon={<FaFemale color={pink} size="25px" />} />
+        <BottomNavigationAction showLabel='true' label="Home" icon={<FaFemale color={pink} size={navFontSize} />} />
       </StyledLink>
       <StyledLink to='/events'>
-        <BottomNavigationAction showLabel='true' label="Events" icon={<PeopleIcon />} />
+        <BottomNavigationAction showLabel='true' label="Events" icon={<MdPeople color={pink} size={navFontSize} />} />
       </StyledLink>
       <StyledLink to='/books'>
-        <BottomNavigationAction showLabel='true' label="Books" icon={<MenuBookIcon />} />
+        <BottomNavigationAction showLabel='true' label="Books" icon={<GiWhiteBook color={pink} size={navFontSize} />} />
       </StyledLink>
       <StyledLink to='/newsletter'>
-        <BottomNavigationAction showLabel='true' label="NewsLetter" icon={<EmailIcon />} />
+        <BottomNavigationAction showLabel='true' label="NewsLetter" icon={<MdEmail color={pink} size={navFontSize} />} />
       </StyledLink>
     </BottomNavigation>
   );
