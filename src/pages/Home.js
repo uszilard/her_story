cdd8e7e5ace3caa3dbc7aa8a368f793b172b25f2
snufka <52801250+snufka@users.noreply.github.com>
2020-03-09 21:55:52 +0100

    home page improvements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,45 +35,59 @@ const homeStyles = makeStyles({
 
 function Home() {
   const classes = homeStyles();
-  const quotes = [{
-    name: "Nancy Regan, Former First Lady of the United States",
-    img: "/nancy.PNG",
-    quote: `“A woman is like a tea bag. She only knows her strength when put in hot water.”`
-  },
-  {
-    name: "Malala Yousafzai, Pakistani activist and Nobel laureate",
-    img: "/malala.jpg",
-    quote:
-      '"We realize the importance of our voices only when we are silenced."'
-  },
-  {
-    name: "Simone de Beauvoir, french author and philosopher",
-    img: "/simone.jpeg",
-    quote: '"One is not born, but rather becomes, a woman."'
-  },
-  {
-    name: "Florence Nightingale, the founder of modern nursing",
-    img: "/florence.jpeg",
-    quote:
-      '"I attribute my success to this: I never gave or took an excuse."'
-  },
-  {
-    name: "Janis Joplin, American musician",
-    img: "/janis.jpeg",
-    quote: '"Don’t compromise yourself. You are all you’ve got."'
-  },
-  {
-    name: "Sojourner Truth, American abolitionist",
-    img: "/sojojpg.jpg",
-    quote:
-      '"If the first woman God ever made was strong enough to turn the world upside down all alone, these together ought to be able to turn it back and get it right side up again."'
-  },
-  {
-    name: "Grace Hopper, U.S. Naval Admiral and computer scientist",
-    img: "/grace.jpg",
-    quote:
-      `"If it's a good idea, go ahead and do it.It's much easier to apologize than it is to get permission."`
-  }
+  const quotes = [
+    {
+      name: "Michelle Obama, Former First Lady of the United States",
+      img: "/obama.png",
+      quote: `"Whether you come from a council estate or a country estate, your success will be determined by your own confidence and fortitude"`
+
+    },
+
+    {
+      name: "Nancy Regan, Former First Lady of the United States",
+      img: "/nancy.PNG",
+      quote: `“A woman is like a tea bag. She only knows her strength when put in hot water.”`
+    },
+    {
+      name: "Malala Yousafzai, Pakistani activist and Nobel laureate",
+      img: "/malala.jpg",
+      quote:
+        '"We realize the importance of our voices only when we are silenced."'
+    },
+    {
+      name: "Simone de Beauvoir, french author and philosopher",
+      img: "/simone.jpeg",
+      quote: '"One is not born, but rather becomes, a woman."'
+    },
+    {
+      name: "Florence Nightingale, the founder of modern nursing",
+      img: "/florence.jpeg",
+      quote:
+        '"I attribute my success to this: I never gave or took an excuse."'
+    },
+    {
+      name: "Janis Joplin, American musician",
+      img: "/janis.png",
+      quote: '"Don’t compromise yourself. You are all you’ve got."'
+    },
+    {
+      name: "Sojourner Truth, American abolitionist",
+      img: "/sojojpg.jpg",
+      quote:
+        '"If the first woman God ever made was strong enough to turn the world upside down all alone, these together ought to be able to turn it back and get it right side up again."'
+    },
+    {
+      name: "Dolly Parton, American singer-songwriter",
+      img: "/dolly.png",
+      quote: `"If your actions create a legacy that inspires others to dream more, learn more, do more and become more, then you are an excellent leader."`
+
+    },
+    {
+      name: "Grace Hopper, U.S. Naval Admiral and computer scientist",
+      img: "/grace.jpg",
+      quote:
+        `"If it's a good idea, go ahead and do it.It's much easier to apologize than it is to get permission."`
+    }
   ];
   return (
     <React.Fragment>
